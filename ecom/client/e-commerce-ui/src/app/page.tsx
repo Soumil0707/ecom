f940778b0d732e4c6d2e5809ee98415f65523fb3
Soumil0707 <74135801+Soumil0707@users.noreply.github.com>
2026-03-08 00:31:45 +0530

    fix dynamic rendering

--- ecom/client/e-commerce-ui/src/app/page.tsx
+++ ecom/client/e-commerce-ui/src/app/page.tsx
@@ -1,17 +1,21 @@
-import BannerSlider from '@/components/BannerSlider'
-import ProductList from './components/ProductList'
+import BannerSlider from "@/components/BannerSlider";
+import ProductList from "./components/ProductList";
 import { Suspense } from "react";
+
+export const dynamic = "force-dynamic";
+
 const Homepage = () => {
   return (
-    <div className=''>
-      <div className='mt-4 mb-12'> 
+    <div>
+      <div className="mt-4 mb-12">
         <BannerSlider />
       </div>
+
       <Suspense fallback={<div>Loading...</div>}>
         <ProductList />
       </Suspense>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
