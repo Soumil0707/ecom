9f395be0dd5cade6c55ae0d1241a3ec9cf00c137
Soumil0707 <74135801+Soumil0707@users.noreply.github.com>
2026-03-24 21:07:09 +0530

    Update cartStore.ts

--- ecom/client/e-commerce-ui/src/stores/cartStore.ts
+++ ecom/client/e-commerce-ui/src/stores/cartStore.ts
@@ -1,8 +1,8 @@
-import { create } from 'zustand'
+// import { create } from 'zustand'
 
-const useStore = create ((set)=>{
-    cart: [],
-    addToCart: () => ,
-    removeFromCart: ()=> ,
-    clearCart: ()=>
-})
+// const useStore = create ((set)=>{
+//     cart: [],
+//     addToCart: () => ,
+//     removeFromCart: ()=> ,
+//     clearCart: ()=>
+// })
